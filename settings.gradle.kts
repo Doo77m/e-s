faf6275626f970f79f31b4846c1d460d25de02f3
Doo77m <136246995+Doo77m@users.noreply.github.com>
2025-11-17 21:11:15 +0300

    Fix1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,21 @@
 /**
  * Add or remove modules to load as needed for local development here.
  */
-loadAllIndividualExtensions()
-// loadIndividualExtension("all", "mangadex")
+// loadAllIndividualExtensions()
+loadIndividualExtension("ar", "mangaswat")
 
 /**
  * ===================================== COMMON CONFIGURATION ======================================
  */
 include(":core")
+include("lib-multisrc:mangathemesia")
+include("lib:i18n")
 
 // Load all modules under /lib
-File(rootDir, "lib").eachDir { include("lib:${it.name}") }
+// File(rootDir, "lib").eachDir { include("lib:${it.name}") }
 
 // Load all modules under /lib-multisrc
-File(rootDir, "lib-multisrc").eachDir { include("lib-multisrc:${it.name}") }
+// File(rootDir, "lib-multisrc").eachDir { include("lib-multisrc:${it.name}") }
 
 /**
  * ======================================== HELPER FUNCTION ========================================
