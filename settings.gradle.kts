ca45b66ed18e6fc488ff27e5e462fbbcee12c0eb
Doo77m <136246995+Doo77m@users.noreply.github.com>
2025-11-18 00:21:09 +0300

    Remove json module from settings
    Removed the json module from included projects.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,6 @@ loadIndividualExtension("ar", "mangaswat")
 include(":core")
 include("lib-multisrc:mangathemesia")
 include("lib:i18n")
-include("lib:json")
 
 // Load all modules under /lib
 // File(rootDir, "lib").eachDir { include("lib:${it.name}") }
